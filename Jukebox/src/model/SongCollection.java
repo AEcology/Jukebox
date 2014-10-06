a411853b6f45f5294f964597e16ac02bffb57698
AEcology <konekotaisha@yahoo.com>
2014-10-05 20:16:06 -0700

    SongCollection editing

--- Jukebox/src/model/SongCollection.java
+++ Jukebox/src/model/SongCollection.java
@@ -1,5 +1,26 @@
 package model;
 
-public class SongCollection {
+import java.util.HashMap;
 
+public class SongCollection {
+	private HashMap<String, Song> songs;
+	
+	public Song getSong(String name){
+		if (!songs.containsKey(name))
+			return null;
+		else
+			return songs.get(name);
+	}
+	public boolean canPlay(String name){
+		if (!songs.containsKey(name))
+			return false;
+		Song sng = songs.get(name);
+		return sng.canPlayToday();
+	}
+	public void resetDay(){
+		for (HashMap.Entry<String,Song> s:songs.entrySet()){
+			s.getValue().reset();
+		}
+	}
+	
 }
